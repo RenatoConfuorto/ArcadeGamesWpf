0d88d451ff99f73131e71265c8677f8087651c4c
Renato Confuorto <renato.confuorto@outlook.it>
2024-02-08 16:48:34 +0100

    Aggiunti interfaccia ISerializableBase e classe SerializableBase

---
+++ Core/Entities/SerializableBase.cs
@@ -0,0 +1,19 @@
+﻿using Core.Interfaces.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public abstract class SerializableBase : ISerializableBase
+    {
+        public int GetSize()
+        {
+            return this.Serialize().Length;
+        }
+        public abstract byte[] Serialize();
+        public abstract void Deserialize(byte[] data);
+    }
+}
