6e0765620cf063be4048bd2cc81b34a083e2cb6a
Renato Confuorto <renato.confuorto@outlook.it>
2024-02-11 13:20:17 +0100

    Risolta visualizzazione chat della lobby

--- ArcadeGames/Views/MultiPlayerLobbyView.xaml.cs
+++ ArcadeGames/Views/MultiPlayerLobbyView.xaml.cs
@@ -20,9 +20,46 @@ namespace ArcadeGames.Views
     /// </summary>
     public partial class MultiPlayerLobbyView : UserControl
     {
+        public static double TEXT_MESSAGES_TEXTBLOCK_WIDTH = 0;
+        public static double USER_NAME_TEXTBLOCK_WIDTH = 175;
         public MultiPlayerLobbyView()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+            //LayoutUpdated += OnLayoutUpdated;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            SetTextMessagesColumnWidth();
+        }
+
+        private void SetTextMessagesColumnWidth()
+        {
+            double mainContainerWidth = MainContainer.ActualWidth;
+            double mainContainerMargin = MainContainer.Margin.Left + MainContainer.Margin.Right;
+            double scrollBarWidth = SystemParameters.ScrollWidth + 30; //Scrollbar width
+
+            TEXT_MESSAGES_TEXTBLOCK_WIDTH = 
+                mainContainerWidth 
+                - mainContainerMargin 
+                - scrollBarWidth 
+                - USER_NAME_TEXTBLOCK_WIDTH - 
+                25; // Label ": "
+            //this.UpdateLayout();
+        }
+
+        private void OnLayoutUpdated(object sender, EventArgs e)
+        {
+        }
+
+        private void TextBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBlock tb && TEXT_MESSAGES_TEXTBLOCK_WIDTH != 0)
+            {
+                tb.Width    = TEXT_MESSAGES_TEXTBLOCK_WIDTH;
+                tb.MaxWidth = TEXT_MESSAGES_TEXTBLOCK_WIDTH;
+            }
         }
     }
 }
